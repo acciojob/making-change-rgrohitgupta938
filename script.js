7734ff6bc5000f70a14cccdaa77feb817bb7b515
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-03 12:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const makeChange = (c) => {
   // your name here
+	const quarterValue = 0.25;
+    const dimeValue = 0.10;
+    const nickelValue = 0.05;
+    const pennyValue = 0.01;
+    let quarters = Math.floor(amount / quarterValue);
+    amount %= quarterValue;
+    let dimes = Math.floor(amount / dimeValue);
+    amount %= dimeValue;
+    let nickels = Math.floor(amount / nickelValue);
+    amount %= nickelValue;
+    let pennies = Math.round(amount / pennyValue);
+    return { q: quarters, d: dimes, n: nickels, p: pennies };
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+// const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
